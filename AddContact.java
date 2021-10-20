4ecbb1db5b474aca1435fb6b08e491380ab637fd
harish29k <90303660+harish29k@users.noreply.github.com>
2021-10-20 08:12:30 +0530

    adding details

--- AddContact.java
+++ AddContact.java
@@ -1,5 +1,34 @@
 package addressbook;
+ 
+import java.util.Scanner;
 
-public class AddContact {
+public class AddContact extends PersonDetail {
+	Scanner scanData = new Scanner(System.in);
 
+	public void SetContactDetails() {
+		System.out.println("enter the First Name");
+		setFirstName(scanData.nextLine());
+		System.out.println("enter the Last Name");
+		setLastName(scanData.nextLine());
+		System.out.println("enter the Address Name");
+		setAddress(scanData.nextLine());
+		System.out.println("enter the State Name");
+		setState(scanData.nextLine());
+		System.out.println("enter the Phone Number");
+		setPhoneNumber(scanData.nextLine());
+		System.out.println("enter the Zip/postal Code");
+		setZipCode(scanData.nextLine());
+		System.out.println("enter the Email");
+		setE_mail(scanData.nextLine());
+	}
+
+	public void getContactDetails() {
+		getFirstName();
+		getLastName();
+		getAddress();
+		getState();
+		getPhoneNumber();
+		getZipCode();
+		getE_mail();
+	}
 }
