f0a0cf752a4b7bfeee0b16421d2658e74010f02d
harish29k <90303660+harish29k@users.noreply.github.com>
2021-10-20 07:50:54 +0530

    creating person details

---
+++ ContactDetails.java
@@ -0,0 +1,19 @@
+package addressbook;
+
+
+public class ContactDetails extends PersonDetail {
+	public static void main(String[] args) {
+		ContactDetails onePersonDetails = new ContactDetails();
+		onePersonDetails.getName();
+		onePersonDetails.getLastName();
+		onePersonDetails.getAddress();
+		onePersonDetails.getStateName();
+		onePersonDetails.getPhoneNumber();
+		onePersonDetails.getEmail();
+		onePersonDetails.getZipCode();
+		System.out.println(onePersonDetails.toString());
+		
+	}
+
+}
+
